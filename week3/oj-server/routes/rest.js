ff6204740052b3dce320bac11b13f0c3a03650f9
Noble <guolei1.chen@gmail.com>
2018-10-29 20:32:04 -0400

    add nginx to make load balancing

--- week3/oj-server/routes/rest.js
+++ week3/oj-server/routes/rest.js
@@ -9,7 +9,7 @@ const nodeRestClient = require('node-rest-client').Client;
 
 const restClient = new nodeRestClient();
 
-EXECUTOR_SERVER_URL ='http://localhost:5000/build_and_run';
+EXECUTOR_SERVER_URL ='http://executor/build_and_run';
 
 restClient.registerMethod('build_and_run', EXECUTOR_SERVER_URL, 'POST');
 
